6ef7488ed3475f096d125f082f28500fad26defe
Jan <j.nhb@web.de>
2026-09-23 19:21:41 +0200

    feat: add x-xsrf-token to Access-Control-Allow-Headers in CORS middleware

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -31,7 +31,7 @@ class CorsMiddleware
             $response->headers->set('Access-Control-Allow-Origin', $origin);
             $response->headers->set('Access-Control-Allow-Credentials', 'true');
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Api-Key, X-Requested-With, Accept, Origin');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Api-Key, X-Requested-With, Accept, Origin, x-xsrf-token');
             $response->headers->set('Access-Control-Max-Age', '86400');
             $response->headers->set('Vary', 'Origin');
         }
